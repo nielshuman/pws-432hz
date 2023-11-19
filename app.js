9dae2790819420021de1806d57c2812861b0b5fb
Niels <nielsz@pm.me>
2023-11-19 14:02:03 +0100

    add sound shit

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-
-const views = $$('.view');
-
 let currentView = 0;
+const views = $$('.view');
+views[currentView].style.display = 'flex';
+views[currentView].classList.add('active');
+
+
 function nextView() {
     if (currentView < views.length - 1) {
         switchView(currentView + 1);
+        return true;
+    } else {
+        return false;
     }
 }
 
-switchView = (n) => {
+const switchView = (n) => {
+    Essential_Audio.Stop()
     views[currentView].classList.remove('active');
     console.log(views[currentView].style.transition)
     setTimeout(() => {
         views[currentView].style.display = 'none';
         currentView = n;
-        views[n].style.display = 'block';
+        views[n].style.display = 'flex';
         setTimeout(() => {
             views[n].classList.add('active');
+            Essential_Audio.init();
         }, 100);
     }, 400);
 };
