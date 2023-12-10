81019066b55e087c92c2aa75e379b7f65b0d3f6f
Niels <nielsz@pm.me>
2023-12-10 14:32:44 +0100

    Fix Safari compatibility and add preloading for audio elements

--- www/app.js
+++ www/app.js
@@ -7,9 +7,13 @@ const views = $$('.view');
 views[currentView].style.display = 'flex';
 views[currentView].classList.add('active');
 
-var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 if (!isSafari) {
-    
+    let scriptEle = document.createElement("script");
+    scriptEle.setAttribute("src", "https://code.getmdl.io/1.3.0/material.min.js");
+    document.body.appendChild(scriptEle);
+    $('#audioa').dataset.preload = true;
+    $('#audiob').dataset.preload = true;
 }
 
 
